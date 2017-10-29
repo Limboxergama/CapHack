fac92b10dbabff6850f98cec0355e5e5bee99ad4
RedCap <redcap@redcaphub.ru>
2017-10-29 16:06:36 +0000

    Debil

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '134.0.118.143'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'CapHack_Base_001'),
+            'username' => env('DB_USERNAME', 'Limboxergama'),
+            'password' => env('DB_PASSWORD', 'Fhcty2001SexStar'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
